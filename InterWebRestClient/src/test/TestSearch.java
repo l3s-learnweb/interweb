99d8384cd7a114e4b9b30185f37315efebefff53
Sergej Zerr <szerr@l3s.de>
2011-10-20 10:38:25 +0000

    git-svn-id: https://svn.l3s.uni-hannover.de/learnweb/trunk/InterWebJ@1526 422a2c9f-66af-4668-9993-60dc3b0871b8
    Former-commit-id: f4e617c754c2a413a4c03550d9099f717209d08d

--- InterWebRestClient/src/test/TestSearch.java
+++ InterWebRestClient/src/test/TestSearch.java
@@ -12,59 +12,62 @@ public class TestSearch
 	
 	public static void main(String[] args)
 	{
-		try
-		{
+		
 			
-			InterWeb interweb = new InterWebJImpl("http://out.l3s.uni-hannover.de:9111/iwj/api/",
+		InterWeb interweb = new InterWebJImpl("***REMOVED***_test/api/",
 			                                      "***REMOVED***",
 			                                      "***REMOVED***");
 			
-			//	InterWeb interweb = new InterWebJImpl("http://out.l3s.uni-hannover.de:9111/iwj/api/", "***REMOVED***", "***REMOVED***");
 			
 			//InterWeb interweb = new InterWebImpl("http://athena.l3s.uni-hannover.de:8000/api/", "71ed49ec4185a50cb0f47257c0d56e0104b7be4cb", "57e6bd162ffb2af298bbab930681870a");
 			TreeMap<String, String> params = new TreeMap<String, String>();
 			
-			String services = "Flickr,YouTube,InterWeb,SlideShare,Google";
+			String services = "Flickr,YouTube,SlideShare,Google";
 			
-			String mediatypes = "audio,presentation,text,video";
+			String mediatypes = "image,presentation,text,video";
 			
 			params.put("media_types", mediatypes);
 			params.put("number_of_results", "10"); //pro service
-			params.put("parallel", "1");
+		//	params.put("parallel", "1");
 			params.put("services", services);
 			String query = "whale";
 			
-			InputStream xmlstream = interweb.searchAsXML(query, params);
-			
-			BufferedReader br = new BufferedReader(new InputStreamReader(xmlstream));
-			String line;
-			while ((line = br.readLine()) != null)
-			{
-				System.out.println(line);
-			}
-			
-			/*
-			 * Alternatively use the built in parser
-			 */
-			SearchQuery parsedquery = interweb.search(query, params);
-			
-			List<SearchResult> results = parsedquery.getResults();
-			if (results.size() == 0)
-			{
-				System.err.print("No results!");
-			}
-			for (SearchResult result : results)
-			{
-				System.out.println(result.getServiceName() + ":"
-				                   + result.getTitle());
-			}
-			
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-		}
-		
+			doSearch("whale",interweb,params);
+		doSearch("whale",interweb,params);
+		doSearch("tree",interweb,params);
 	}
+
+	private static void doSearch(String query, InterWeb interweb, TreeMap<String, String> params) {
+		try{
+		InputStream xmlstream = interweb.searchAsXML(query, params);
+	
+	BufferedReader br = new BufferedReader(new InputStreamReader(xmlstream));
+	String line;
+	while ((line = br.readLine()) != null)
+	{
+		System.out.println(line);
+	}
+	
+	/*
+	 * Alternatively use the built in parser
+	 */
+	SearchQuery parsedquery = interweb.search(query, params);
+	
+	List<SearchResult> results = parsedquery.getResults();
+	if (results.size() == 0)
+	{
+		System.err.print("No results!");
+	}
+	for (SearchResult result : results)
+	{
+		System.out.println(result.getServiceName() + ":"
+		                   + result.getTitle());
+	}
+	
+}
+catch (Exception e)
+{
+	e.printStackTrace();
+}}
 	
 }
