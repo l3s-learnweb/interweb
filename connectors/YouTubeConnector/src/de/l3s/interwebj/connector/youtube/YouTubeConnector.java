6903ac37d3d1719513a872f02f8c88ee42ec9e16
Jaspreet Singh <singh@l3s.de>
2012-08-27 15:00:13 +0000

    git-svn-id: https://svn.l3s.uni-hannover.de/learnweb/trunk/InterWebJ@1925 422a2c9f-66af-4668-9993-60dc3b0871b8
    Former-commit-id: ca6a35b716ff4003651339cb578765499ff77ee3

--- connectors/YouTubeConnector/src/de/l3s/interwebj/connector/youtube/YouTubeConnector.java
+++ connectors/YouTubeConnector/src/de/l3s/interwebj/connector/youtube/YouTubeConnector.java
@@ -58,6 +58,8 @@ import de.l3s.interwebj.query.QueryResult;
 import de.l3s.interwebj.query.ResultItem;
 import de.l3s.interwebj.query.Thumbnail;
 import de.l3s.interwebj.query.UserSocialNetworkResult;
+import de.l3s.interwebj.socialsearch.SocialSearchQuery;
+import de.l3s.interwebj.socialsearch.SocialSearchResult;
 import de.l3s.interwebj.util.CoreUtils;
 
 
@@ -678,6 +680,14 @@ public class YouTubeConnector
 	@Override
 	public UserSocialNetworkResult getUserSocialNetwork(String userid,
 			AuthCredentials authCredentials) throws InterWebException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public SocialSearchResult get(SocialSearchQuery query,
+			AuthCredentials authCredentials) {
 		// TODO Auto-generated method stub
 		return null;
 	}
