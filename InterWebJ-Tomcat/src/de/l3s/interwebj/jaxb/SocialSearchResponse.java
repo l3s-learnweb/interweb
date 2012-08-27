f372dd67e480e5f052e7e92e433c7c49be84bd1c
Jaspreet Singh <singh@l3s.de>
2012-08-27 15:03:17 +0000

    git-svn-id: https://svn.l3s.uni-hannover.de/learnweb/trunk/InterWebJ@1930 422a2c9f-66af-4668-9993-60dc3b0871b8
    Former-commit-id: 1df624d4be71397f3a46631c0baea9d89d4d1ad2

---
+++ InterWebJ-Tomcat/src/de/l3s/interwebj/jaxb/SocialSearchResponse.java
@@ -0,0 +1,51 @@
+package de.l3s.interwebj.jaxb;
+
+
+import javax.xml.bind.annotation.*;
+
+import de.l3s.interwebj.query.*;
+import de.l3s.interwebj.socialsearch.SocialSearchQuery;
+import de.l3s.interwebj.socialsearch.SocialSearchResult;
+import de.l3s.interwebj.socialsearch.SocialSearchResultItem;
+
+
+@XmlRootElement(name = "socialsearchresponse")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class SocialSearchResponse
+    extends XMLResponse
+{
+	
+	@XmlElement(name = "query")
+	protected SocialSearchQueryEntity query;
+	
+
+	public SocialSearchResponse()
+	{
+	}
+	
+
+	public SocialSearchResponse(SocialSearchResult queryResult)
+	{
+		SocialSearchQueryEntity searchQueryEntity = new SocialSearchQueryEntity(queryResult.getQuery());
+		
+		for (SocialSearchResultItem resultItem : queryResult.getResultItems())
+		{
+			SocialSearchResultEntity iwSearchResult= new SocialSearchResultEntity(resultItem);
+			
+			searchQueryEntity.addResult(iwSearchResult);
+		}
+		query = searchQueryEntity;
+	}
+	
+
+	public SocialSearchQueryEntity getQuery()
+	{
+		return query;
+	}
+	
+
+	public void setQuery(SocialSearchQueryEntity query)
+	{
+		this.query = query;
+	}
+}
---
+++ InterWebJ-Tomcat/src/de/l3s/interwebj/rest/SocialSearch.java
@@ -0,0 +1,123 @@
+package de.l3s.interwebj.rest;
+
+
+import java.text.ParseException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
+
+import de.l3s.interwebj.InterWebException;
+import de.l3s.interwebj.core.Engine;
+import de.l3s.interwebj.core.Environment;
+import de.l3s.interwebj.core.InterWebPrincipal;
+import de.l3s.interwebj.jaxb.ErrorResponse;
+import de.l3s.interwebj.jaxb.SearchResponse;
+import de.l3s.interwebj.jaxb.SocialNetworkResponse;
+import de.l3s.interwebj.jaxb.XMLResponse;
+import de.l3s.interwebj.query.Query;
+import de.l3s.interwebj.query.Query.SearchScope;
+import de.l3s.interwebj.query.Query.SortOrder;
+import de.l3s.interwebj.query.QueryFactory;
+import de.l3s.interwebj.query.QueryResult;
+import de.l3s.interwebj.query.QueryResultCollector;
+import de.l3s.interwebj.query.UserSocialNetworkCollector;
+import de.l3s.interwebj.query.UserSocialNetworkResult;
+import de.l3s.interwebj.socialsearch.SocialSearchQuery;
+import de.l3s.interwebj.socialsearch.SocialSearchResult;
+import de.l3s.interwebj.socialsearch.SocialSearchResultCollector;
+import de.l3s.interwebj.util.CoreUtils;
+import de.l3s.interwebj.util.ExpirableMap;
+
+
+@Path("/socialsearch")
+public class SocialSearch
+    extends Endpoint
+{
+	
+	@Context
+	HttpServletRequest request;
+	@Context
+	private UriInfo uriInfo;
+	
+
+	@GET
+	@Produces(MediaType.APPLICATION_XML)
+	public XMLResponse getSocialNetworkResult(@QueryParam("userid") String userid,
+			@QueryParam("q") String queryString,
+            @QueryParam("date_from") String dateFrom,
+            @QueryParam("date_till") String dateTill,
+            @QueryParam("ranking") String ranking,
+            @QueryParam("number_of_results") String resultCount,
+            @QueryParam("services") String services,
+            @QueryParam("page") String page,
+            @QueryParam("timeout") String timeout)
+	                                  
+	{
+		
+		
+		try
+		{
+
+			Engine engine = Environment.getInstance().getEngine();
+			InterWebPrincipal principal = getPrincipal();
+			Environment.logger.info("principal: [" + principal + "]");
+			SocialSearchQuery query= new SocialSearchQuery(queryString);
+			SocialSearchResultCollector collector = engine.getSocialSearchResultsOf(query.getQuery(), principal);
+			SocialSearchResult queryResult = collector.retrieve();
+			
+			ExpirableMap<String, Object> expirableMap = engine.getExpirableMap();
+			expirableMap.put(queryResult.getQuery(), queryResult);
+			SearchResponse searchResponse = new SearchResponse(queryResult);
+			String userName = (principal == null)
+			    ? "anonymous" : principal.getName();
+			searchResponse.getQuery().setUser(userName);
+			Environment.logger.info(searchResponse.getQuery().getResults().size()
+			                        + " results found in "
+			                        + searchResponse.getQuery().getElapsedTime()
+			                        + " ms");
+			return searchResponse;
+		}
+		catch (InterWebException e)
+		{
+			e.printStackTrace();
+			Environment.logger.severe(e.getMessage());
+			return new ErrorResponse(999, e.getMessage());
+		}
+	}
+	
+
+	
+	private static ErrorResponse checkServices(Query query, String services)
+	{
+		Engine engine = Environment.getInstance().getEngine();
+		if (services == null || services.trim().length() == 0)
+		{
+			List<String> connectorNames = engine.getConnectorNames();
+			for (String connectorName : connectorNames)
+			{
+				query.addConnectorName(connectorName);
+			}
+			return null;
+		}
+		String[] serviceArray = services.split(",");
+		List<String> connectorNames = engine.getConnectorNames();
+		for (String service : serviceArray)
+		{
+			if (connectorNames.contains(service))
+			{
+				query.addConnectorName(service);
+			}
+		}
+		return null;
+	}
+}
