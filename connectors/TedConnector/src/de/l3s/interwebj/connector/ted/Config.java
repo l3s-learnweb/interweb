85e3be8ff0adc1833689792de9dc1d7473f58682
Oleh Astappiev <astappiev@l3s.de>
2020-05-10 19:21:06 +0200

    refactor: remove deprecated connectors
    Former-commit-id: 742aef36aa8e2c64d063c9f742bb85e5741124cb [formerly d3fd0c3fc6e0230be1462d2d18e205576d1ab3a7]
    Former-commit-id: 465d3400133a846d10b11af683b694eea9ca5ae7

--- connectors/TedConnector/src/de/l3s/interwebj/connector/ted/Config.java
+++
@@ -1,8 +0,0 @@
-package de.l3s.interwebj.connector.ted;
-
-public class Config {
-	
-	public static final String GRAPH_NAME="http://data-observatory.org/ted_talks";
-	public static final String QUERY_ENDPOINT="http://meco.l3s.uni-hannover.de:8890/sparql";
-
-}
