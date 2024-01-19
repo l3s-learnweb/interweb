d5658dd8447b4bc7b87676976e020c691ec70946
Oleh Astappiev <oleh@astappiev.me>
2024-01-19 11:36:59 +0100

    fix: youtube date convertor

--- interweb-core/src/main/java/de/l3s/interweb/core/util/DateUtils.java
+++ interweb-core/src/main/java/de/l3s/interweb/core/util/DateUtils.java
@@ -1,6 +1,7 @@
 package de.l3s.interweb.core.util;
 
 import java.time.Instant;
+import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
@@ -10,6 +11,9 @@ import de.l3s.interweb.core.ConnectorException;
 
 public final class DateUtils {
     private static final DateTimeFormatter PARSE_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd[ ]['T']HH:mm:ss[.SSSSSSS][XXX][ z]").withZone(ZoneOffset.UTC);
+
+    private DateUtils() {
+    }
 
     public static Instant parse(String str) {
         if (str == null) return null;
@@ -26,6 +30,6 @@ public final class DateUtils {
     }
 
     public static String toRfc3339(TemporalAccessor temporal) {
-        return temporal == null ? null : DateTimeFormatter.ISO_INSTANT.format(temporal);
+        return temporal == null ? null : DateTimeFormatter.ISO_INSTANT.withZone(ZoneId.systemDefault()).format(temporal);
     }
 }
