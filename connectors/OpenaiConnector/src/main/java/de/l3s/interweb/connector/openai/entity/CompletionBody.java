f1b86354fe6a33475463d60c52acb10107dbce08
ChristophScn <christoph@hoebbel-schnell.de>
2024-06-28 14:36:04 +0200

    Adds additional parameters for completions API

--- connectors/OpenaiConnector/src/main/java/de/l3s/interweb/connector/openai/entity/CompletionBody.java
+++ connectors/OpenaiConnector/src/main/java/de/l3s/interweb/connector/openai/entity/CompletionBody.java
@@ -2,11 +2,14 @@ package de.l3s.interweb.connector.openai.entity;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import io.quarkus.runtime.annotations.RegisterForReflection;
 
 import de.l3s.interweb.core.completion.CompletionQuery;
+import de.l3s.interweb.core.completion.ResponseFormat;
 
 @RegisterForReflection
 public final class CompletionBody {
@@ -27,6 +30,19 @@ public final class CompletionBody {
     @JsonProperty("max_tokens")
     private Integer maxTokens;
 
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Integer n;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Integer seed;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonProperty("response_format")
+    private ResponseFormat responseFormat;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String[] stop;
+
     public CompletionBody(CompletionQuery query) {
         this.messages = query.getMessages().stream().map(CompletionMessage::new).toList();
         this.temperature = query.getTemperature();
@@ -34,6 +50,10 @@ public final class CompletionBody {
         this.frequencyPenalty = query.getPresencePenalty();
         this.presencePenalty = query.getPresencePenalty();
         this.maxTokens = query.getMaxTokens();
+        this.n = query.getN();
+        this.seed = query.getSeed();
+        this.responseFormat = query.getResponseFormat();
+        this.stop = query.getStop();
     }
 
     public List<CompletionMessage> getMessages() {
@@ -58,5 +78,21 @@ public final class CompletionBody {
 
     public Integer getMaxTokens() {
         return maxTokens;
+    }
+
+    public Integer getN() {
+        return n;
+    }
+
+    public Integer getSeed() {
+        return seed;
+    }
+
+    public ResponseFormat getResponseFormat() {
+        return responseFormat;
+    }
+
+    public String[] getStop() {
+        return stop;
     }
 }
