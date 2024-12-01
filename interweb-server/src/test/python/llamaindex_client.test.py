815ddbc624a1972c1081bd3255ba1b2206e2f7a9
Oleh Astappiev <oleh@astappiev.me>
2024-12-01 19:58:45 +0100

    feat: add llamaindex completion example

--- interweb-server/src/test/python/llamaindex_client.test.py
+++ interweb-server/src/test/python/llamaindex_client.test.py
@@ -1,9 +1,21 @@
 import os
 from llama_index.embeddings.openai import OpenAIEmbedding
+from llama_index.llms.openai_like import OpenAILike
+from llama_index.core.llms import ChatMessage, MessageRole
 
 os.environ["OPENAI_API_BASE"] = os.getenv("INTERWEB_HOST", "https://interweb.l3s.uni-hannover.de/v1")
 os.environ["OPENAI_API_KEY"] = os.getenv("INTERWEB_APIKEY")
 
+# Create a completion
+llm = OpenAILike(model="llama3.1:8b", is_chat_model=True) # gpt-4o-mini
+
+resp = llm.chat([
+    ChatMessage(role=MessageRole.SYSTEM, content="Always answer the question, even if the context isn't helpful."),
+    ChatMessage(role=MessageRole.USER, content="What is your mission?"),
+])
+print(resp)
+
+# Get Embeddings
 embed_model = OpenAIEmbedding(
     model_name="bge-m3:567m"
 )
