873c12489407417f7fd0a6ad7841d7fb7b0b1dc4
Oleh Astappiev <astappiev@l3s.de>
2025-04-08 17:34:05 +0200

    feat: add Vue3 frontend via Quinoa

---
+++ interweb-server/src/main/webui/vite.config.js
@@ -0,0 +1,25 @@
+import {fileURLToPath, URL} from 'node:url'
+
+import {defineConfig} from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+    base: '',
+    server: {
+        // this ensures that the browser opens upon server start
+        open: true,
+        // this sets a default port to 3000, you can change this
+        port: 3000,
+    },
+    plugins: [
+        vue(),
+        vueDevTools(),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        },
+    },
+})
