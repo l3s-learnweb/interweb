01e7bb1ce9ae293c5466e106b37f09127599f571
Oleh Astappiev <4512729+astappiev@users.noreply.github.com>
2025-08-19 12:59:28 +0200

    fix: ollama params

--- connectors/OllamaConnector/src/main/java/de/l3s/interweb/connector/ollama/entity/OllamaModelOptions.java
+++ connectors/OllamaConnector/src/main/java/de/l3s/interweb/connector/ollama/entity/OllamaModelOptions.java
@@ -12,27 +12,50 @@ import de.l3s.interweb.core.chat.CompletionsQuery;
 public class OllamaModelOptions {
     @JsonProperty("num_ctx")
     private Integer numCtx;
-    @JsonProperty("repeat_penalty")
-    private Double repeatPenalty;
-    private Double temperature;
-    private Integer seed;
-    private String stop;
     @JsonProperty("num_predict")
     private Integer numPredict;
+    @JsonProperty("num_keep")
+    private Integer numKeep;
+    @JsonProperty("repeat_last_n")
+    private Double repeatLastN;
+    // @JsonProperty("num_batch")
+    // private Integer numBatch;
+    // @JsonProperty("num_gpu")
+    // private Integer numGpu;
+    // @JsonProperty("main_gpu")
+    // private Integer mainGpu;
+    // @JsonProperty("use_mmap")
+    // private Boolean useMmap;
+    // @JsonProperty("num_thread")
+    // private Integer numThread;
+    // private Boolean numa;
+    @JsonProperty("repeat_penalty")
+    private Double repeatPenalty;
+    @JsonProperty("presence_penalty")
+    private Double presencePenalty;
+    @JsonProperty("frequency_penalty")
+    private Double frequencyPenalty;
+    @JsonProperty("penalize_newline")
+    private Boolean penalizeNewline;
+    private Double temperature;
+    private Integer seed;
+    private String[] stop;
     @JsonProperty("top_p")
     private Double topP;
     @JsonProperty("min_p")
     private Double minP;
+    @JsonProperty("typical_p")
+    private Double typicalP;
     @JsonProperty("top_k")
     private Integer topK;
 
     public OllamaModelOptions(CompletionsQuery query) {
         this.seed = query.getSeed();
         if (query.getStop() != null && query.getStop().length > 0) {
-            this.stop = query.getStop()[0];
+            this.stop = query.getStop();
         }
-        this.repeatPenalty = query.getFrequencyPenalty();
-        this.numPredict = query.getMaxTokens();
+        this.presencePenalty = query.getPresencePenalty();
+        this.frequencyPenalty = query.getFrequencyPenalty();
         if (this.numCtx == null && query.getMaxTokens() != null && query.getMaxTokens() >= 2048) {
             this.numCtx = query.getMaxTokens();
         } else {
@@ -48,8 +71,32 @@ public class OllamaModelOptions {
         return numCtx;
     }
 
+    public Integer getNumPredict() {
+        return numPredict;
+    }
+
+    public Integer getNumKeep() {
+        return numKeep;
+    }
+
+    public Double getRepeatLastN() {
+        return repeatLastN;
+    }
+
     public Double getRepeatPenalty() {
         return repeatPenalty;
+    }
+
+    public Double getPresencePenalty() {
+        return presencePenalty;
+    }
+
+    public Double getFrequencyPenalty() {
+        return frequencyPenalty;
+    }
+
+    public Boolean getPenalizeNewline() {
+        return penalizeNewline;
     }
 
     public Double getTemperature() {
@@ -60,12 +107,8 @@ public class OllamaModelOptions {
         return seed;
     }
 
-    public String getStop() {
+    public String[] getStop() {
         return stop;
-    }
-
-    public Integer getNumPredict() {
-        return numPredict;
     }
 
     public Double getTopP() {
@@ -74,6 +117,10 @@ public class OllamaModelOptions {
 
     public Double getMinP() {
         return minP;
+    }
+
+    public Double getTypicalP() {
+        return typicalP;
     }
 
     public Integer getTopK() {
